32778cb0309cef50b8d9debe02a76866f1b6e47f
AzizLike29 <abdulfirdaus590@gmail.com>
2023-01-22 22:57:49 +0700

    menambahkan fitur INSERT data

--- kuliah/pertemuan 10/latihan3.php
+++ kuliah/pertemuan 10/latihan3.php
@@ -11,6 +11,9 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Daftar Mahasiswa</title>
+
+  <a href="tambah.php">Tambah Data Mahasiswa</a>
+  <br><br>
 </head>
 
 <body>
